f4fb837ab57daf30f09d5f690a33c27677735fb1
BigBarfEaterBBE <ajiang9@bostonk12.org>
2026-05-05 17:31:15 -0400

    5/5 5:31 PM I altered the timing sensitivity. Instead of just checking if its before or after market time, I'm seeing if it is before or after and then using that to wait until open or stopping the program if after hours. Then I did some refactoring and reorganizing and simplification of my code to make it more readable and improvable later on. iN MY REFACTOR I ALSO GOT RID OF A LOT OF THE UNECESSARY MEAN REVERSION CODE THAT WAS STILL IN THE PULLBACK SCRIPT.

--- pullback.py
+++ pullback.py
@@ -22,30 +22,57 @@ import time
 
 # LOAD API INFO
 import confidential
-MR_API_KEY = confidential.MR_API_KEY
-MR_SECRET_KEY = confidential.MR_SECRET
 PB_API_KEY = confidential.PB_API_KEY
 PB_SECRET_KEY = confidential.PB_SECRET
 
+# CONFIGURATION
+TICKERS = ["NVDA", "GOOGL", "SPY", "DIA", "QQQ"]
+RISK_PERCENT = 0.05
+TIMEZONE = ZoneInfo("America/New_York")
 
-# INITIALIZING TRADING CLIENT
-mr_client = TradingClient(api_key = MR_API_KEY, secret_key=MR_SECRET_KEY, paper=True)
+# INITIALIZING CLIENTS
 pb_client = TradingClient(api_key = PB_API_KEY, secret_key=PB_SECRET_KEY, paper=True)
+stock_data_client = StockHistoricalDataClient(PB_API_KEY, PB_SECRET_KEY)
 
-# INTIALIZING MARKET DATA CLIENT
-stock_data_client = StockHistoricalDataClient(MR_API_KEY, MR_SECRET_KEY)
+# STATE TRACKING
+partial_taken_pb = {ticker: False for ticker in TICKERS}
+last_trade_time = {ticker: None for ticker in TICKERS}
+data_map = {}
+
+# HELPER FUNCTIONS
+def now():
+    return datetime.now(TIMEZONE)
+
+def get_market_status():
+    now = now(),
+    current_time = now.time()
+
+    open_time = dt_time(9,30)
+    close_time = dt_time(16,0)
+
+    if current_time < open_time:
+        return "before_open"
+    elif open_time <= current_time <= close_time:
+        return "open"
+    else:
+        return "after_close"
 
 
-def market_open():
-    now = datetime.now(ZoneInfo("America/New_York")).time()
-    return dt_time(9,30) <= now <= dt_time(16,0)
+def sleep_until_open():
+    now = now(),
+    open_dt = now.replace(hour = 9, minute = 30, second = 0, microsecond=0)
+
+    sleep_seconds = (open_dt - now).total_seconds()
+
+    print(f"Market opens in {sleep_seconds/60:.1f} minutes... sleeping")
+    time.sleep(max(sleep_seconds, 0))
 
 def log_trade(ticker, action, price, qty, strategy, profit=None):
     with open("trades.csv", mode="a", newline="") as file:
         writer = csv.writer(file)
 
         writer.writerow([
-            datetime.now(),
+            now(),
             ticker,
             strategy,
             action,
@@ -53,24 +80,23 @@ def log_trade(ticker, action, price, qty, strategy, profit=None):
             qty,
             profit
         ])
+def log_equity(client, name):
+    account = client.get_account()
 
-def get_data(ticker):
-    """
-    Gets data for a stock from the past 60 days
-    """
-    now = datetime.now(ZoneInfo("America/New_York"))
+    equity = float(account.equity)
+    cash = float(account.cash)
 
-    req = StockBarsRequest(symbol_or_symbols=[ticker],
-                           timeframe=TimeFrame(amount=5,unit=TimeFrameUnit.Minute), 
-                           start=now-timedelta(days=5)
-    )
+    with open("equity.csv", mode="a", newline="") as file:
+        writer = csv.writer(file)
 
-    bars_df = stock_data_client.get_stock_bars(req).df
-    
-    # Fix MultiIndex
-    df= bars_df.xs(ticker)
-    return df
+        writer.writerow([
+            now(),
+            name,
+            equity,
+            cash
+        ])
 
+# INDICATORS
 def add_indicators(df):
     df['pct_change'] = df['close'].pct_change() * 100
 
@@ -91,12 +117,30 @@ def add_indicators(df):
     df['vol_spike'] = df['volume'] > df['vol_avg'] * 1.5
 
     df['pb_signal'] = (
-        ((df['drop_3'] <= -0.015) # large drop (-1.5% in 15 min candle)
+        (df['drop_3'] <= -0.005) # large drop (-1.5% in 15 min candle)
         | (df['vwap_dev'] <= -0.01)
-        | (df['pct_change'] <= -1.5) )
-        & df['vol_spike'] # shows increase in volume -> panic selling
+        | (df['pct_change'] <= -1.5)
+        | df['vol_spike'] # shows increase in volume -> panic selling
     ) 
 
+    return df
+
+# DATA
+def get_data(ticker):
+    """
+    Gets data for a stock from the past 60 days
+    """
+    now = now()
+
+    req = StockBarsRequest(symbol_or_symbols=[ticker],
+                           timeframe=TimeFrame(amount=5,unit=TimeFrameUnit.Minute), 
+                           start=now-timedelta(days=5)
+    )
+
+    bars_df = stock_data_client.get_stock_bars(req).df
+    
+    # Fix MultiIndex
+    df= bars_df.xs(ticker)
     return df
 
 def get_position(client, ticker):
@@ -106,75 +150,9 @@ def get_position(client, ticker):
     except:
         return 0,0
 
-def buy_stock(client, ticker, amount):
-    amount = round(amount, 2)
-    order = MarketOrderRequest(symbol=ticker, notional=amount, side=OrderSide.BUY, time_in_force=TimeInForce.DAY)
-    client.submit_order(order)
-
-def sell_stock(client,ticker):
-    try:
-        pos = client.get_open_position(ticker)
-
-        order = MarketOrderRequest(symbol=ticker, qty=float(pos.qty), side=OrderSide.SELL, time_in_force=TimeInForce.DAY)
-        client.submit_order(order)
-    except:
-        pass
-
-def log_equity(client, name):
-    account = client.get_account()
-
-    equity = float(account.equity)
-    cash = float(account.cash)
-
-    with open("equity.csv", mode="a", newline="") as file:
-        writer = csv.writer(file)
-
-        writer.writerow([
-            datetime.now(),
-            name,
-            equity,
-            cash
-        ])
-
-def sell_partial(client, ticker, fraction):
-    try:
-        pos = client.get_open_position(ticker)
-        qty = float(pos.qty)
-        
-        sell_qty = round(qty * fraction, 6)
-
-        order = MarketOrderRequest(
-            symbol=ticker,
-            qty=sell_qty,
-            side=OrderSide.SELL,
-            time_in_force = TimeInForce.DAY
-        )
-        client.submit_order(order)
-    except:
-        pass
-
-# MAIN TRADING LOGIC
-tickers = ["NVDA", "GOOGL", "SPY", "DIA", "QQQ"]
-risk_percent = 0.05
-
-
-# ADD PARTIAL TRACKING
-partial_taken_mr = {ticker: False for ticker in tickers}
-partial_taken_pb = {ticker: False for ticker in tickers}
-
-# PREVENT DUPE TRADES ON SAME CANDLE
-last_trade_time = {ticker: None for ticker in tickers}
-
-# DATA
-data_map = {}
-for ticker in tickers:
-    df = add_indicators(get_data(ticker))
-    data_map[ticker] = df
-    df = data_map[ticker]
-
 def update_data(ticker, df):
     last_time = df.index[-1]
-    now = datetime.now(ZoneInfo("America/New_York"))
+    now = now(),
     req = StockBarsRequest(
         symbol_or_symbols=[ticker],
         timeframe=TimeFrame(amount=5, unit=TimeFrameUnit.Minute),
@@ -197,16 +175,60 @@ def update_data(ticker, df):
     df = df.tail(300) # keep last 300 candles
     return df
 
+# TRADING
+
+def buy_stock(client, ticker, amount):
+    amount = round(amount, 2)
+    order = MarketOrderRequest(symbol=ticker, notional=amount, side=OrderSide.BUY, time_in_force=TimeInForce.DAY)
+    client.submit_order(order)
+
+def sell_stock(client,ticker):
+    try:
+        pos = client.get_open_position(ticker)
+
+        order = MarketOrderRequest(symbol=ticker, qty=float(pos.qty), side=OrderSide.SELL, time_in_force=TimeInForce.DAY)
+        client.submit_order(order)
+    except:
+        pass
+
+def sell_partial(client, ticker, fraction):
+    try:
+        pos = client.get_open_position(ticker)
+        qty = float(pos.qty)
+        
+        sell_qty = round(qty * fraction, 6)
+
+        order = MarketOrderRequest(
+            symbol=ticker,
+            qty=sell_qty,
+            side=OrderSide.SELL,
+            time_in_force = TimeInForce.DAY
+        )
+        client.submit_order(order)
+    except:
+        pass
+
+
+# GET DATA
+for ticker in TICKERS:
+    df = add_indicators(get_data(ticker))
+    data_map[ticker] = df
+    df = data_map[ticker]
+
+# TRADING LOOP
 
 while True:
-    if not market_open():
-        time.sleep(300)
-        continue
+    status = get_market_status()
+    if status == "before_open":
+        sleep_until_open()
+    elif status == "after_close":
+        print("Market closed. Stopping bot.")
+        break
     else:
-        mr_balance = float(mr_client.get_account().cash)
+        print("Market open. Running algorithm")
         pb_balance = float(pb_client.get_account().cash)
 
-        for ticker in tickers:
+        for ticker in TICKERS:
             data_map[ticker] = update_data(ticker, data_map[ticker])
             df = data_map[ticker]
             current_time = df.index[-1]
@@ -221,11 +243,10 @@ while True:
             price = row['close']
 
             shares, avg_entry = get_position(pb_client, ticker)
-            pb_balance = float(pb_client.get_account().cash)
 
             # BUY
             if shares == 0 and row['pb_signal']:
-                trade_amount = pb_balance * risk_percent
+                trade_amount = pb_balance * RISK_PERCENT
                 buy_stock(pb_client, ticker, trade_amount)
                 shares, avg_entry = get_position(pb_client, ticker)
                 last_trade_time[ticker] = current_time
@@ -236,25 +257,24 @@ while True:
             # SCALE
             elif shares > 0:
                 drop = (price- avg_entry) / avg_entry
-                if drop <= -0.06:
-                    trade_amount = pb_balance * risk_percent * 2
+                if drop <= -0.006:
+                    trade_amount = pb_balance * RISK_PERCENT * 2
                     buy_stock(pb_client, ticker, trade_amount)
                     last_trade_time[ticker] = current_time
                     shares, avg_entry = get_position(pb_client, ticker)
                     log_trade(ticker, "ADD", price, trade_amount, "PULLBACK_TREND")
-                elif drop <= -0.04:
-                    trade_amount = pb_balance * risk_percent * 1.5
+                elif drop <= -0.004:
+                    trade_amount = pb_balance * RISK_PERCENT * 1.5
                     buy_stock(pb_client, ticker, trade_amount)
                     last_trade_time[ticker] = current_time
                     shares, avg_entry = get_position(pb_client, ticker)
                     log_trade(ticker, "ADD", price, trade_amount, "PULLBACK_TREND")
-                elif drop <= -0.02:
-                    trade_amount = pb_balance * risk_percent
+                elif drop <= -0.002:
+                    trade_amount = pb_balance * RISK_PERCENT
                     buy_stock(pb_client, ticker, trade_amount)
                     last_trade_time[ticker] = current_time
                     shares, avg_entry = get_position(pb_client, ticker)
                     log_trade(ticker, "ADD", price, trade_amount, "PULLBACK_TREND")
-            pb_balance = float(pb_client.get_account().cash)
             # SELL
             if shares > 0:
                 atr = row['atr']
